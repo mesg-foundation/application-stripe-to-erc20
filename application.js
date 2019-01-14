b0841ebc906b8ebc1f050c94df60a96dbb38a403
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2019-01-14 17:59:25 +0300

    update code to meet with mesg-js v2

--- application.js
+++ application.js
@@ -1,60 +1,71 @@
-const MESG = require('mesg-js').application()
+const { application } = require('mesg-js')
 const BigNumber = require('bignumber.js')
+
+const mesg = application()
 
 const TOKEN_PRICE = 0.4
 const ERC20_ADDRESS = '0xd14A3D6b94016e455af5eB7F329bc572EA626c5F'
 const ERC20_DECIMALS = BigNumber(10).pow(18)
 const emails = {}
 
-MESG.whenEvent({ serviceID: 'webhook' }, {
-  serviceID: 'stripe',
-  taskKey: 'charge',
-  inputs: (event, { data }) => {
+mesg.listenEvent({ serviceID: 'webhook' })
+  .on('data', (event) => {
     console.log("Receiving webhook => Charging on Stripe")
+    const data = JSON.parse(event.eventData)
     emails[data.ethAddress.toUpperCase()] = data.email
-    return {
-      amount: data.number * TOKEN_PRICE * 100,
-      currency: 'usd',
-      email: data.email,
-      metadata: {
-        address: data.ethAddress,
-        tokens: data.number
-      },
-      source: data.token,
-      stripeSecretKey: process.env.STRIPE_SECRET
-    }
-  }
-})
+    MESG.executeTask({
+      serviceID: 'stripe',
+      taskKey: 'charge',
+      inputData: JSON.stringify({ 
+        amount: data.number * TOKEN_PRICE * 100,
+        currency: 'usd',
+        email: data.email,
+        metadata: {
+          address: data.ethAddress,
+          tokens: data.number
+        },
+        source: data.token,
+        stripeSecretKey: process.env.STRIPE_SECRET
+      })
+    }).catch((err) => console.log(err.message))
+  })
+  .on('error', (err) => console.log(err.message))
 
-MESG.whenEvent({ serviceID: 'stripe', eventKey: 'charged' }, {
-  serviceID: 'ethereum-erc20-ropsten',
-  taskKey: 'transfer',
-  inputs: (event, { metadata }) => {
+mesg.listenEvent({ serviceID: 'stripe', eventFilter: 'charged' })
+  .on('data', (event) => {
     console.log("Stripe payment confirmed => Transfering ERC20")
-    return {
-      contractAddress: ERC20_ADDRESS,
-      privateKey: process.env.PRIVATE_KEY,
-      gasLimit: 100000,
-      to: metadata.address,
-      value: (metadata.tokens * ERC20_DECIMALS).toString()
-    }
-  }
-})
+    const metadata = JSON.parse(event.eventData)
+    MESG.executeTask({
+      serviceID: 'ethereum-erc20-ropsten',
+      taskKey: 'transfer',
+      inputData: JSON.stringify({
+        contractAddress: ERC20_ADDRESS,
+        privateKey: process.env.PRIVATE_KEY,
+        gasLimit: 100000,
+        to: metadata.address,
+        value: (metadata.tokens * ERC20_DECIMALS).toString()
+      })
+    }).catch((err) => console.log(err.message))
+  })
+  .on('error', (err) => console.log(err.message))
 
-MESG.whenEvent({ serviceID: 'ethereum-erc20-ropsten', eventKey: 'transfer', filter: (event, data) => {
-  return data.contractAddress.toUpperCase() === ERC20_ADDRESS.toUpperCase()
-    && data.to && emails[data.to.toUpperCase()]
-} }, {
-  serviceID: 'email-sendgrid',
-  taskKey: 'send',
-  inputs: (event, { to, value, transactionHash }) => {
-    console.log("ERC20 received => Sending email")
-    return {
-      apiKey: process.env.SENDGRID_API_KEY,
-      from: 'contact@mesg.com',
-      to: emails[to.toUpperCase()],
-      subject: `Your MESG tokens just arrived`,
-      text: `Hello, you just received your ${BigNumber(value).dividedBy(ERC20_DECIMALS).toString()} MESG tokens. See the details of the transaction here https://ropsten.etherscan.io/tx/${transactionHash}`
+mesg.listenEvent({ serviceID: 'ethereum-erc20-ropsten', eventFilter: 'transfer' })
+.on('data', (event) => {
+  console.log("ERC20 received => Sending email")
+  const transfer = JSON.parse(event.eventData)
+  if (transfer.contractAddress.toUpperCase() === ERC20_ADDRESS.toUpperCase()
+    && transfer.to && emails[transfer.to.toUpperCase()]) {
+      MESG.executeTask({
+        serviceID: 'email-sendgrid',
+        taskKey: 'send',
+        inputData: JSON.stringify({
+          apiKey: process.env.SENDGRID_API_KEY,
+          from: 'contact@mesg.com',
+          to: emails[transfer.to.toUpperCase()],
+          subject: `Your MESG tokens just arrived`,
+          text: `Hello, you just received your ${BigNumber(transfer.value).dividedBy(ERC20_DECIMALS).toString()} MESG tokens. See the details of the transaction here https://ropsten.etherscan.io/tx/${transfer.transactionHash}`
+        })
+      }).catch((err) => console.log(err.message))
     }
-  }
 })
+.on('error', (err) => console.log(err.message))
